e802c8b6dfbc2328019891cbaf491fcd4ac88a4b
phanbien2004 <bien382004@gmail.com>
2025-06-07 05:11:17 +0000

    fix

--- model/AuthenticationModel.js
+++ model/AuthenticationModel.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/sharing_photo");
+mongoose.connect(
+  "mongodb+srv://username123:PRQuKf4ZayNEvCKN@cluster.ncqn9r4.mongodb.net/photosharing?retryWrites=true&w=majority&appName=Cluster"
+);
 
 const AuthenticationSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    userId: String,
-})
+  username: String,
+  password: String,
+  userId: String,
+});
 
 module.exports = mongoose.model("authentications", AuthenticationSchema);
